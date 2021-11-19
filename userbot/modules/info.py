a68be6166a52e02cbb81ec2b858afaa5f86b78b7
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-20 01:57:22 +0400

    Update info.py

--- userbot/modules/info.py
+++ userbot/modules/info.py
@@ -4,9 +4,8 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
 
-from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, ASENA_VERSION
+from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, GOLDEN_VERSION
 from userbot.events import register
 from telethon import version
 from platform import python_version
