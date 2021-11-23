03e9a60008f02266a43de407258a72b52b5ef553
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-23 20:46:07 +0400

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -145,7 +145,7 @@ def extractCommands(file):
 try:
     bot.start()
     idim = bot.get_me().id
-    asenabl = requests.get('https://gitlab.com/Quiec/asen/-/raw/master/asen.json').json()
+    asenabl = requests.get('https://gitlab.com/Emin-ahmedoff/gold/-/raw/main/goldenblacklist.json').json()
     if idim in asenabl:
         bot.disconnect()
 
