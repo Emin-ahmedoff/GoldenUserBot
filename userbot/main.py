4e20588f82192d6891b521aaf7d022e48bfd6105
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-23 21:03:02 +0400

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -64,7 +64,7 @@ AFKSTR = [
 UNAPPROVED_MSG = ("`Hey,` {mention}`! Bu bir bot. Narahat olma.\n\n`"
                   "`Sahibim sənə PM atma icazəsi verməyib. `"
                   "`Zəhmət olmasa sahibimin aktiv olmağını gözləyin, o adətən PM'ləri qəbul edir.\n\n`"
-                  "`Bildiyim qədəri ilə o dəlilərə PM atma icazəsi vermir.`\n@AlphaUserBot")
+                  "`Bildiyim qədəri ilə o dəlilərə PM atma icazəsi vermir.`\n@GoldenUserBot")
 
 DB = connect("learning-data-root.check")
 CURSOR = DB.cursor()
