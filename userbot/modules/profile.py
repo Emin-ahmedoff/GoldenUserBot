cc7628d0a2466a6e7393d57916b88abb907615d9
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-25 15:42:42 +0400

    Update profile.py

--- userbot/modules/profile.py
+++ userbot/modules/profile.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Golden userbot
 
 
 """ Telegram'daki profil detaylarınızı değişmeye yarayan UserBot modülüdür. """
