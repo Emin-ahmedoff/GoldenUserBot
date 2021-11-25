d76929b8f61b05b888b1b62b82cc99b066611903
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-25 15:58:34 +0400

    Update covid19.py

--- userbot/modules/covid19.py
+++ userbot/modules/covid19.py
@@ -21,41 +21,37 @@ import flag
 from userbot.cmdhelp import CmdHelp
 
 @register(outgoing=True, pattern="^.covid ?(.*)$")
-@register(outgoing=True, pattern="^.korona ?(.*)$")
 async def covid(event):
     try:
         if event.pattern_match.group(1) == '':
             country = 'AZ'
-        else:
+        else: 
             country = event.pattern_match.group(1)
 
         bayrak = flag.flag(country)
         worldData = get('https://coronavirus-19-api.herokuapp.com/all').json()
-        countryData = get(
-            'https://coronavirus-19-api.herokuapp.com/countries/' +
-            pytz.country_names[country]).json()
-    except BaseException:
+        countryData = get('https://coronavirus-19-api.herokuapp.com/countries/' + pytz.country_names[country]).json()
+    except:
         await event.edit(LANG['SOME_ERRORS'])
         return
 
-    sonuclar = (
-        f"** {LANG['DATA']}**\n" +
-        f"\n**{LANG['EARTH']}**\n" +
-        f"**{LANG['CASE']}** `{worldData['cases']}`\n" +
-        f"**{LANG['DEATH']}** `{worldData['deaths']}`\n" +
-        f"**{LANG['HEAL']}** `{worldData['recovered']}`\n" +
-        f"\n**{pytz.country_names[country]}**\n" +
-        f"**{bayrak} {LANG['AZ_ALL_CASES']}** `{countryData['cases']}`\n" +
-        f"**{bayrak} {LANG['AZ_CASES']}** `{countryData['todayCases']}`\n" +
-        f"**{bayrak} {LANG['AZ_CASE']}** `{countryData['active']}`\n" +
-        f"**{bayrak} {LANG['AZ_ALL_DEATHS']}** `{countryData['deaths']}`\n" +
-        f"**{bayrak} {LANG['AZ_DEATHS']}** `{countryData['todayDeaths']}`\n" +
-        f"**{bayrak} {LANG['AZ_HEAL']}** `{countryData['recovered']}`\n" +
-        f"**{bayrak} Test Sayısı:** `{countryData['totalTests']}`")
+    sonuclar = (f"** {LANG['DATA']}**\n" +
+                f"\n**{LANG['EARTH']}**\n" +
+                f"**{LANG['CASE']}** `{worldData['cases']}`\n" +
+                f"**{LANG['DEATH']}** `{worldData['deaths']}`\n" +
+                f"**{LANG['HEAL']}** `{worldData['recovered']}`\n" +
+                f"\n**{pytz.country_names[country]}**\n" +
+                f"**{bayrak} {LANG['AZ_ALL_CASES']}** `{countryData['cases']}`\n" +
+                f"**{bayrak} {LANG['AZ_CASES']}** `{countryData['todayCases']}`\n" +
+                f"**{bayrak} {LANG['AZ_CASE']}** `{countryData['active']}`\n" +
+                f"**{bayrak} {LANG['AZ_ALL_DEATHS']}** `{countryData['deaths']}`\n" +
+                f"**{bayrak} {LANG['AZ_DEATHS']}** `{countryData['todayDeaths']}`\n" +
+                f"**{bayrak} {LANG['AZ_HEAL']}** `{countryData['recovered']}`\n" +
+                f"**{bayrak} Test Sayı:** `{countryData['totalTests']}`"
+                )
     await event.edit(sonuclar)
 
 CmdHelp('covid19').add_command(
-    'covid',
-    '<ülke kodu>',
-    'Həm dünyada, həm də təqdim etdiyiniz ölkə üçün mövcud Covid 19 statistikası. Ölkəniz fərqlidirsə, onu əmrin yanına əlavə edin.',
-    'covid az -> Azerbaycanı getirir.').add_warning('`Ölkə yazmasaz Azerbaycanı secər.`').add()
+    'covid', '<ölkə kodu>', 'Həm Dünya geneli həm də verdiyiniz ölkə üçün güncəl Covid 19 statistikası. Ölkəniz fərqlidirsə komandanın yanına əlavə etməyiniz bəsdir.',
+    'covid tr -> Türkiyəni gətirər.'
+).add_warning('`Ölkə yazmasanız Azərbaycanı seçər.`').add()
