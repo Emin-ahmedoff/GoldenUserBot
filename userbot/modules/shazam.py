72d01cc2cedcbe3e417ab4854799bf67dcb0eef2
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-25 15:39:15 +0400

    Update shazam.py

--- userbot/modules/shazam.py
+++ userbot/modules/shazam.py
@@ -1,13 +1,8 @@
-# Copyright (C) 2020 Yusuf Usta.
-#
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
-#
 
-# Asena UserBot - Yusuf Usta
-# Telegram @Fusuf
 
-# Coded by @Fusuf
+#Golden userbot
 
 from pydub import AudioSegment
 from json import dumps
