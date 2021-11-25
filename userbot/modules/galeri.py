37f265f7a90f2fef00b6dcb65a293c9ad1d3e679
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-25 16:04:13 +0400

    Update galeri.py

--- userbot/modules/galeri.py
+++ userbot/modules/galeri.py
@@ -1,10 +1,9 @@
-# Copyright (C) 2020 Yusuf Usta.
-#
+
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Golden Userbot
 
 import re
 import asyncio
