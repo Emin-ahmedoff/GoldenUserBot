bbb86e90821f9050bfa427575ad8ec7977d2926d
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-11-28 14:06:29 +0400

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# /A L P H A / U S E R / B O T/
+# Golden Userbot
 
 """ Hadiseleri idare etmek üçün User Bot modulu.
  UserBot'un esas komponentlerinden biri. """
