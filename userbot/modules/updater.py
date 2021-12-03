02f159f8f1b0eb0b212caf9f76f539fe8a465035
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-12-03 17:47:49 +0400

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -159,7 +159,7 @@ async def upstream(ups):
 
 
 @register(husu=True, pattern="^Golden ən son versiyaya(?: |$)(.*)")
-async def upstream(ups):
+async def (ups):
     conf = ups.pattern_match.group(1)
     if ups.is_reply:
         reply = await ups.get_reply_message()
