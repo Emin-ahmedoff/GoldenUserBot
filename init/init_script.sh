99752fb9e0265aec8b1dce1dbb4eb78c6d686a0c
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2021-12-11 01:04:51 +0400

    Update init_script.sh

--- init/init_script.sh
+++ init/init_script.sh
@@ -48,7 +48,7 @@ botclone() {
     cd ~
     echo "Cloning bot sources..."
     if [ -z "$bot_clone" ]; then
-        git clone https://github.com/Emin-ahmedoff/GoldenUserBot -b main
+        git clone https://github.com/Emin-ahmedoff/GoldenUserBot -b master
         save_prog "bot_clone"
     fi
     echo "DONE!!"
