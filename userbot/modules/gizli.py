737c4d1eab444e5462135f8ff2cd2800169bf469
Emin-ahmedoff <91464064+Emin-ahmedoff@users.noreply.github.com>
2022-02-18 01:47:03 +0400

    Update gizli.py

--- userbot/modules/gizli.py
+++ userbot/modules/gizli.py
@@ -14,7 +14,7 @@ async def wspr(event):
     if event.fwd_from:
         return
     wwwspr = event.pattern_match.group(1)
-    botusername = "@whisperbot"
+    botusername = "@nnbbot"
     if event.reply_to_msg_id:
         reply_to_id = await event.get_reply_message()
     tap = await bot.inline_query(botusername, wwwspr) 
